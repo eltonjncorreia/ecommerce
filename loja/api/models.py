a69aeb336ec876e6ddc8deabddfb62115f09c13f
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-22 21:11:10 -0300

    modelos

--- loja/api/models.py
+++ loja/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Produto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    nome = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE,
+                                  related_name='categoria_do_produto')
+
+    def __str__(self):
+        return f'{self.nome}, {self.descricao}'
+
+
+class Pedido(models.Model):
+    STATUS = (('PR', 'Pedido Realizado'),
+              ('SP', 'Separação em estoque'),
+              ('EM', 'Em montagem'),
+              ('RT', 'Realização de testes'),
+              ('CO', 'Concluido'))
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    produto = models.ManyToManyField('Produto', related_name='produtos_em_pedidos')
+    preco = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
+    status = models.CharField(max_length=2, choices=STATUS, default='PR')
+
+    def __str__(self):
+        return f'{self.produto}'
+
+
+class Categoria(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.nome}'
+
+
+class Estoque(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    item = models.ForeignKey('Produto', on_delete=models.CASCADE, related_name='produto_em_estoque')
+    quantidade = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.item}, {self.quantidade}'
--- loja/api/views.py
+++ loja/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ProdutoSerializer
+from .models import Produto
 
-# Create your views here.
+
+class ProdutoViewSet(APIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+    @classmethod
+    def get(cls, request):
+        url = 'https://api.github.com/users'
+        response = requests.get(url)
+        usuarios = response.json()
+        serializer = UserGithubSerializer(usuarios, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
