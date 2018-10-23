29f60bad18a9a2c192ff538d11febd0eb5dc7edb
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-22 21:52:39 -0300

    rotas da api

--- loja/api/urls.py
+++ loja/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProdutoView
+from .views import ProdutoView, CategoriaView
 
 urlpatterns = [
-    path('api/', ),
+    path('v1/produtos/', ProdutoView.as_view(), name="produtos"),
+    path('v1/categorias/', CategoriaView.as_view(), name="categorias"),
 
 ]
