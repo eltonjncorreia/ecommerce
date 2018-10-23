9d525b37a0df203b0b972a75b36980a0868b90de
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-22 21:52:18 -0300

    serializando modelos

---
+++ loja/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Produto, Categoria, Estoque, Pedido
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+
+class EstoqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estoque
+        fields = '__all__'
+
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
