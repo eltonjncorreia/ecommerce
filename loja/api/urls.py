57ce0744cb9af49a73e31a3fdf9cff835d2afc92
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-23 21:54:53 -0300

    rota por id

--- loja/api/urls.py
+++ loja/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProdutoView, CategoriaView
+from .views import ProdutoView, ProdutoDetailView, CategoriaView
 
 urlpatterns = [
     path('v1/produtos/', ProdutoView.as_view(), name="produtos"),
+    path('v1/produtos/<uuid:pk>/', ProdutoDetailView.as_view(), name="produtos-detail"),
     path('v1/categorias/', CategoriaView.as_view(), name="categorias"),
 
 ]
