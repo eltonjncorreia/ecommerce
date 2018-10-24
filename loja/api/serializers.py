deda6b533048bdca4e9df332b701aefbd106fec2
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-23 21:54:14 -0300

    serializers

--- loja/api/serializers.py
+++ loja/api/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
+
 from .models import Produto, Categoria, Estoque, Pedido
-
-
-class ProdutoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Produto
-        fields = '__all__'
 
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
+        fields = '__all__'
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Produto
         fields = '__all__'
 
 
